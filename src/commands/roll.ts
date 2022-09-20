d8a5f7a84fd8203a41de20951971c9a27d47926f
Ozoku <19756856+ozokuz@users.noreply.github.com>
2022-09-20 10:39:12 +0300

    made roll command dry

--- src/commands/roll.ts
+++ src/commands/roll.ts
@@ -1,11 +1,9 @@
 import { ChatInputCommandInteraction, EmbedBuilder } from 'discord.js';
 import { command } from '../utils';
 
-interface Options {
-  isPrivate: boolean;
-  rpgNotation?: string;
-  sides?: number;
-  amount?: number;
+interface Dice {
+  sides: number;
+  amount: number;
 }
 
 // Interface for dice roll results
@@ -31,7 +29,7 @@ const diceRoll = (sides: number) => {
 };
 
 // Function to extract amount and size of dice to roll from rpg notation
-const parseRPGNotation = (rpgSyntax: string): [number, number] | undefined => {
+const parseRPGNotation = (rpgSyntax: string): Dice | undefined => {
   // Extract dice amount and sides
   const result = rpgNotationRegex.exec(rpgSyntax);
 
@@ -39,37 +37,31 @@ const parseRPGNotation = (rpgSyntax: string): [number, number] | undefined => {
   if (!result) return;
 
   // Get the amount and sides from the result
-  let [, amountStr, sizeStr] = result;
+  let [, amountStr, sidesStr] = result;
   // Remove undefined
   amountStr = amountStr ?? '';
-  sizeStr = sizeStr ?? '';
+  sidesStr = sidesStr ?? '';
   // If amount is empty only one dice is being rolled
   if (amountStr === '') amountStr = '1';
 
   // parse the amount string to a number
   const amount = parseInt(amountStr);
-  const size = parseInt(sizeStr);
+  const sides = parseInt(sidesStr);
 
-  return [amount, size];
+  return { amount, sides };
 };
 
-const rollWithRPGNotation = async (
+const getDiceFromRPGNotation = async (
   interaction: ChatInputCommandInteraction,
-  options: Options,
-) => {
-  const { isPrivate, rpgNotation } = options;
-  const embed = new EmbedBuilder();
+  embed: EmbedBuilder,
+  rpgNotation: string,
+): Promise<Dice[] | void> => {
   // Function to reply with an error message
   const sendError = async (message: string) => {
     embed.setDescription(message);
 
     await interaction.reply({ ephemeral: true, embeds: [embed] });
   };
-
-  if (!rpgNotation)
-    return await sendError(
-      "Error NB1763: This shouldn't happen, please inform @Ozoku",
-    );
 
   // Don't accept massive amouts of dice
   const isTooLong = rpgNotation.length > 50;
@@ -78,34 +70,51 @@ const rollWithRPGNotation = async (
   // Splitting the text to individual dice rolls
   const dice = rpgNotation.split(' ');
 
-  // Verify that every dice roll's syntax is valid
+  // Verify that the notation is valid for each dice roll
   if (!dice.every((die) => rpgNotationRegex.test(die)))
     return await sendError('Invalid dice syntax');
+
+  // Parse the notations
+  const parsedDice = dice.map((die) => parseRPGNotation(die));
+
+  // Filter out invalid dice
+  return parsedDice.filter((die) => !!die) as Dice[];
+};
+
+const rollDice = async (
+  interaction: ChatInputCommandInteraction,
+  embed: EmbedBuilder,
+  dice: Dice[],
+): Promise<boolean> => {
+  // Function to reply with an error message
+  const sendError = async (message: string) => {
+    embed.setDescription(message);
+
+    await interaction.reply({ ephemeral: true, embeds: [embed] });
+
+    return false;
+  };
 
   // List to store rolls in
   const rolls = new Array<Roll>();
 
   // Loop through all the dice rolls
   for (const die of dice) {
-    // Parse dice syntax
-    const parsedDiceRoll = parseRPGNotation(die);
-    if (!parsedDiceRoll) return await sendError('Invalid dice syntax');
-
     // Extract amount and size
-    const [amount, size] = parsedDiceRoll;
+    const { amount, sides } = die;
 
     // Error handling
     if (amount === 0)
       return await sendError('How am I supposed to roll zero dice?');
-    if (size === 2)
+    if (sides === 2)
       return await sendError('Did you mean to flip a coin instead?');
-    if (size === 1)
+    if (sides === 1)
       return await sendError(
         "A 1 sided dice? that's just 1, every single time",
       );
     if (amount > 100)
       return await sendError("I don't have that many dice to roll");
-    if (size > 1000)
+    if (sides > 1000)
       return await sendError('How big of a dice are you trying to roll?');
 
     // List to store results in
@@ -113,11 +122,11 @@ const rollWithRPGNotation = async (
 
     // Roll the dice
     for (let i = 0; i < amount; i++) {
-      results.push(diceRoll(size));
+      results.push(diceRoll(sides));
     }
 
     // Store the results
-    rolls.push({ dice: `${amount} x D${size}`, results: results.join(', ') });
+    rolls.push({ dice: `${amount} x D${sides}`, results: results.join(', ') });
   }
 
   embed.setTitle('Roll Results:');
@@ -126,55 +135,7 @@ const rollWithRPGNotation = async (
     embed.addFields({ name: roll.dice, value: roll.results }),
   );
 
-  await interaction.reply({ ephemeral: isPrivate, embeds: [embed] });
-};
-
-const rollWithBasicNotation = async (
-  interaction: ChatInputCommandInteraction,
-  options: Options,
-) => {
-  const { isPrivate, sides, amount } = options;
-  const embed = new EmbedBuilder();
-  // Function to reply with an error message
-  const sendError = async (message: string) => {
-    embed.setDescription(message);
-
-    await interaction.reply({ ephemeral: true, embeds: [embed] });
-  };
-
-  if (!sides || !amount)
-    return await sendError(
-      "Error NB1764: This shouldn't happen, please inform @Ozoku",
-    );
-
-  // Error handling
-  if (amount === 0)
-    return await sendError('How am I supposed to roll zero dice?');
-  if (sides === 2)
-    return await sendError('Did you mean to flip a coin instead?');
-  if (sides === 1)
-    return await sendError("A 1 sided dice? that's just 1, every single time");
-  if (amount > 100)
-    return await sendError("I don't have that many dice to roll");
-  if (sides > 1000)
-    return await sendError('How big of a dice are you trying to roll?');
-
-  // List to store results in
-  const results = new Array<number>();
-
-  // Roll the dice
-  for (let i = 0; i < amount; i++) {
-    results.push(diceRoll(sides));
-  }
-
-  // Store the results
-  const roll = { dice: `${amount} x D${sides}`, results: results.join(', ') };
-
-  embed.setTitle('Roll Results:');
-
-  embed.addFields({ name: roll.dice, value: roll.results });
-
-  await interaction.reply({ ephemeral: isPrivate, embeds: [embed] });
+  return true;
 };
 
 export default command(
@@ -201,13 +162,35 @@ export default command(
   async (_ctx, interaction) => {
     // Options from user
     const isPrivate = !!interaction.options.getBoolean('private');
-    const rpgNotation = interaction.options.getString('rpg') || undefined;
-    const sides = interaction.options.getNumber('sides') || 6;
-    const amount = interaction.options.getNumber('amount') || 1;
-    const options: Options = { isPrivate, rpgNotation, sides, amount };
+    const rpgNotation = interaction.options.getString('rpg');
+    const sides = interaction.options.getNumber('sides') ?? 6;
+    const amount = interaction.options.getNumber('amount') ?? 1;
 
-    if (rpgNotation) return await rollWithRPGNotation(interaction, options);
+    const embed = new EmbedBuilder();
+    let success = false;
 
-    return await rollWithBasicNotation(interaction, options);
+    // Set basic notation as default data
+    let dice: Dice[] = [{ sides, amount }];
+
+    // Check if we can get data through RPG notation
+    if (rpgNotation) {
+      const rpgNotationDice = await getDiceFromRPGNotation(
+        interaction,
+        embed,
+        rpgNotation,
+      );
+
+      if (!rpgNotationDice) return;
+
+      dice = rpgNotationDice;
+    }
+
+    // Roll the dice
+    success = await rollDice(interaction, embed, dice);
+
+    // If the dice roll wasn't successful, we have already sent a message
+    if (!success) return;
+
+    await interaction.reply({ ephemeral: isPrivate, embeds: [embed] });
   },
 );
