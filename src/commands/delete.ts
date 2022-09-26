247a9d42c75a2a9a6e9257bd712f7c0bfa27cc40
Ozoku <19756856+ozokuz@users.noreply.github.com>
2022-09-26 21:55:44 +0300

    added new command: delete - delete commands, only usable by staff

---
+++ src/commands/delete.ts
@@ -0,0 +1,30 @@
+import { command, Permission } from '../utils';
+
+export default command(
+  (data) =>
+    data
+      .setName('delete')
+      .setDescription('Delete messages')
+      .addNumberOption((option) =>
+        option.setName('amount').setDescription('Amount of messages to delete'),
+      ),
+  async (_ctx, interaction) => {
+    if (!interaction.channel) return;
+    if (interaction.channel.isDMBased()) return;
+    if (!interaction.channel.isTextBased()) return;
+
+    const amount = interaction.options.getNumber('amount') ?? 1;
+
+    await interaction.reply({
+      ephemeral: true,
+      content: `Deleting ${amount} messages...`,
+    });
+
+    await interaction.channel.bulkDelete(amount);
+
+    await interaction.editReply({
+      content: `Successfully deleted ${amount} messages.`,
+    });
+  },
+  Permission.Staff,
+);
